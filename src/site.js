946452219caa71a15990df53b5fc05841645e258
callumf222 <callumfry57@gmail.com>
2022-10-28 10:14:12 +0100

    Main

---
+++ src/site.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+export default function nav() {
+
+  return (
+   <>
+    <div id="menu">
+      <div id="menu-items">
+        <a href ="/" className="menu-item">// Home</a>
+        <a href ="/" className="menu-item">// About me</a>
+        <a href ="/" className="menu-item">// Contact</a>
+        <a href ="/" className="menu-item">// Github</a>
+      </div>
+      <div id="menu-background-image"> </div>
+    </div>
+
+    <div id = "rightHand-side"> 
+          <h1>Callum Fry</h1>
+          
+          <h2>Computer Science student</h2>
+    </div>
+
+    </>
+  )
+}

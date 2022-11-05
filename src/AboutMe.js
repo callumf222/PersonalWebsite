fb3255bb46d390ce0f86e2cf9a67a4fb60dc63bb
Callumf222 <callumfry57@gmail.com>
2022-11-05 19:48:34 +0000

    Text changes complete

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -16,12 +16,12 @@ export default function AboutMe() {
               <p>I am currently studying Computer Science at the University of Portsmouth; subject areas that particularly interest me include programming, software engineering and cyber security.</p>
                
                
-               <p>Throughout my first year I built on my communication and teamwork skills while leadinging a number of group projects.</p>
+               <p>Throughout my first year I built on my communication and teamwork skills while leading a number of group projects.</p>
               
               
               <p>In my spare time I enjoy playing games, watching football and coding; comfortable in Java, Python, HTML, CSS, Javascript, Dart, C# and SQL.</p>
             
-              <p>You can check out some of my projects and work below, as I learn and improve as a developer.</p>
+              <p>Feel free to check out some of my projects below; following my progress in becoming a developer.</p>
 
 
           </div>

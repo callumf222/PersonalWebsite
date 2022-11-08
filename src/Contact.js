33c6230e26daa0827a0f16738f5f44f3c6db96d1
Callumf222 <callumfry57@gmail.com>
2022-11-08 22:56:14 +0000

    icons

--- src/Contact.js
+++ src/Contact.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCoffee} from '@fortawesome/free-solid-svg-icons';
+import {} from '@fortawesome/fontawesome-svg-core'
 
 export default function Contact() {
-
   return (
     <>
 
         <a id='ContactPage'></a>
-        <section className='Contact'>
-          <div>
-            <h1 className='hContact'>Contact</h1>
-          </div>
+        <section className='Contact'>     
+
+          <h1 className='hContact'>Contact Me!</h1>
+
+          <FontAwesomeIcon icon={"fa-brands fa-youtube"} />
+          <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
 
           <div className='backgroundContact'></div>
 

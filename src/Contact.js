1d17dc0e954a157ccb3d167136c742f50445eebd
Callumf222 <callumfry57@gmail.com>
2022-11-13 23:54:58 +0000

    updating icons

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaGithub,FaEnvelope,FaLinkedin} from 'react-icons/fa'
+import { FaGithub,FaEnvelope,FaLinkedin,FaInstagram,FaFacebook} from 'react-icons/fa'
 import { IconContext } from "react-icons";
 import { Link } from '@mui/icons-material';
 
@@ -11,20 +11,30 @@ export default function Contact() {
         <a id='ContactPage'></a>
         <section className='Contact'>     
           <div className='backgroundContact'>
-
+ 
             <h1 className='hContact'>Contact Me!</h1>
 
-            <IconContext.Provider value={{className:"icons", color:"blue", size:"3em"}}>
-              <a href='https://github.com/callumf222'>
-                <FaGithub/>
-              </a>
-              <a href= "mailto:callumfry57@gmail.com ">
-                <FaEnvelope/>
-              </a>
-              <a href='https://www.linkedin.com/in/callum-fry-64bab91a2/'>
-                <FaLinkedin/>
-              </a>
-            </IconContext.Provider>
+            <div className='iconHolder'>
+
+              <IconContext.Provider value={{className:"icons", size:"5em"}}>
+                <a className='githubIcon' href='https://github.com/callumf222'>
+                  <FaGithub/>
+                </a>
+                <a className='emailIcon' href= "mailto:callumfry57@gmail.com ">
+                  <FaEnvelope/>
+                </a>
+                <a className='linkedinIcon' href='https://www.linkedin.com/in/callum-fry-64bab91a2/'>
+                  <FaLinkedin/>
+                </a>
+                <a className='instagramIcon' href='https://www.instagram.com/_callum__fry/'>
+                  <FaInstagram/>
+                </a>
+                <a className='facebookIcon' href='https://www.facebook.com/callum.fry.549'>
+                  <FaFacebook/>
+                </a>
+              </IconContext.Provider>
+
+            </div>
 
           </div>
 

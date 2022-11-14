a054573ebc7dc70848e5c669135dfa34b197ac8d
Callumf222 <callumfry57@gmail.com>
2022-11-14 14:25:20 +0000

    projects

--- src/Projects.js
+++ src/Projects.js
@@ -6,6 +6,7 @@ export default function Project() {
     <>
 
       <a id='ProjectPage'></a>
+      <div className='background'>
       <div className='spacer layer1'>
 
           
@@ -13,49 +14,58 @@ export default function Project() {
           <h1 className='hProjects'>My Projects</h1>
       </section>
 
-      <div class="flip-card">
-        <div class="flip-card-inner">
-          <div class="flip-card-front">
-            <div className='Project-Photo'></div>
-          </div>
-          <div class="flip-card-back">
-            <h1>My Project</h1>
-            <p>Desc</p>
-            <p>Link</p>
+      <div className='projectCards'>
+
+        <div class="flip-card">
+          <div class="flip-card-inner">
+            <div class="flip-card-front">
+              <div className='Project-Photo'></div>
+            </div>
+            <div class="flip-card-back">
+              <h1>My Project</h1>
+              <p>Desc</p>
+              <p>Link</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="flip-card2">
-        <div class="flip-card-inner2">
-          <div class="flip-card-front2">
-            <div className='Project-Photo2'></div>
-          </div>
-          <div class="flip-card-back2">
-            <h1>My Project 2</h1>
-            <p>Desc</p>
-            <p>Link</p>
+        <div class="flip-card2">
+          <div class="flip-card-inner2">
+            <div class="flip-card-front2">
+              <div className='Project-Photo2'></div>
+            </div>
+            <div class="flip-card-back2">
+              <h1>My Project 2</h1>
+              <p>Desc</p>
+              <p>Link</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="flip-card3">
-        <div class="flip-card-inner3">
-          <div class="flip-card-front3">
-            <div className='Project-Photo3'></div>
-          </div>
-          <div class="flip-card-back3">
-            <h1>My Project 3</h1>
-            <p>Desc</p>
-            <p>Link</p>
+        <div class="flip-card3">
+          <div class="flip-card-inner3">
+            <div class="flip-card-front3">
+              <div className='Project-Photo3'></div>
+            </div>
+            <div class="flip-card-back3">
+              <h1>My Project 3</h1>
+              <p>Desc</p>
+              <p>Link</p>
+            </div>
           </div>
         </div>
-      </div>
 
+      </div>
       
 
       </div>
       
+
+
+
+
+      </div>
+
 
     </>
   )

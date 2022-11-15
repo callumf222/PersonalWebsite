40787f1d6569374f05fa75aed8fe56e3ea2e32e3
Callumf222 <callumfry57@gmail.com>
2022-11-15 16:57:16 +0000

    icons in place
    project page

--- src/Projects.js
+++ src/Projects.js
@@ -5,67 +5,23 @@ export default function Project() {
   return (
     <>
 
-      <a id='ProjectPage'></a>
-      <div className='background'>
-      <div className='spacer layer1'>
+      <div className='backgroundProjects'>
+        <div className='spacer layer1'>
 
           
-      <section className='Projects'>     
-          <h1 className='hProjects'>My Projects</h1>
-      </section>
+          <section className='Projects'>     
+              <h1 className='hProjects'>My Projects</h1>
+          </section>
 
-      <div className='projectCards'>
-
-        <div class="flip-card">
-          <div class="flip-card-inner">
-            <div class="flip-card-front">
-              <div className='Project-Photo'></div>
-            </div>
-            <div class="flip-card-back">
-              <h1>My Project</h1>
-              <p>Desc</p>
-              <a href='/'>Link</a>
-            </div>
+          <div className='projectImages'>
+            <a className='projectImage1'></a>
+            <a className='projectImage2'></a>
+            <a className='projectImage3'></a>
           </div>
+
+    
         </div>
-
-        <div class="flip-card2">
-          <div class="flip-card-inner2">
-            <div class="flip-card-front2">
-              <div className='Project-Photo2'></div>
-            </div>
-            <div class="flip-card-back2">
-              <h1>My Project 2</h1>
-              <p>Desc</p>
-              <a href='/'>Link</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="flip-card3">
-          <div class="flip-card-inner3">
-            <div class="flip-card-front3">
-              <div className='Project-Photo3'></div>
-            </div>
-            <div class="flip-card-back3">
-              <h1>My Project 3</h1>
-              <p>Desc</p>
-              <a href='/'>Link</a>
-            </div>
-          </div>
-        </div>
-
       </div>
-      
-
-      </div>
-      
-
-
-
-
-      </div>
-
 
     </>
   )

60d28d1480519170b8267aa3632c910424f2e5cc
Callumf222 <callumfry57@gmail.com>
2022-11-18 11:05:08 +0000

    project icons working!!!

--- src/Projects.js
+++ src/Projects.js
@@ -7,19 +7,22 @@ export default function Project() {
 
       <div className='backgroundProjects'>
         <div className='spacer layer1'>
-
           
           <section className='Projects'>     
               <h1 className='hProjects'>My Projects</h1>
           </section>
 
-             <div className='projectImages'>
-                <a href='/' className='projectImage1'></a>
-                <a href='/' className='projectImage2'></a>
-                <a href='/' className='projectImage3'></a>
-              </div>
+          <div className='projectDesc'>
+            <p>This is my project desribition</p>
+          </div>
 
-              
+          <div className='projectImages'>
+            <a href='/' className='projectImage1'> <h2 className='projectName'>Project Name</h2> </a>
+            <a href='/' className='projectImage2'> <h2 className='projectName'>Project Name</h2> </a>
+            <a href='/' className='projectImage3'> <h2 className='projectName'>Project Name</h2> </a>
+            <a href='/' className='projectImage1'> <h2 className='projectName'>Project Name</h2> </a>
+            <a href='/' className='projectImage2'> <h2 className='projectName'>Project Name</h2> </a>
+          </div>
 
     
         </div>

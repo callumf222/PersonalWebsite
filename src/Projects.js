4d5ef3a58fbbf9ba1db3ee020c0fca98ee3464bc
Callumf222 <callumfry57@gmail.com>
2022-11-21 19:27:24 +0000

    project fixing

--- src/Projects.js
+++ src/Projects.js
@@ -12,16 +12,14 @@ export default function Project() {
               <h1 className='hProjects'>My Projects</h1>
           </section>
 
-          <div className='projectDesc'>
-            <p>This is my project desribition</p>
-          </div>
+         
 
           <div className='projectImages'>
-            <a href='/' className='projectImage1'> <h2 className='projectName'>Project Name</h2> </a>
-            <a href='/' className='projectImage2'> <h2 className='projectName'>Project Name</h2> </a>
-            <a href='/' className='projectImage3'> <h2 className='projectName'>Project Name</h2> </a>
-            <a href='/' className='projectImage1'> <h2 className='projectName'>Project Name</h2> </a>
-            <a href='/' className='projectImage2'> <h2 className='projectName'>Project Name</h2> </a>
+              <a href='/' className='projectImage1'> <h2 className='projectName'>Personal Website</h2> </a>
+              <a href='/' className='projectImage2'> <h2 className='projectName'>Scheduling App</h2> </a>
+              <a href='/' className='projectImage3'> <h2 className='projectName'>Gaming Site</h2> </a>
+              <a href='/' className='projectImage1'> <h2 className='projectName'>Netfix Clone</h2> </a>
+              <a href='/' className='projectImage2'> <h2 className='projectName'></h2> </a>
           </div>
 
     

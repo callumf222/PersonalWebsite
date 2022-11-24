e521888cfeeace7df66c72c10b99a620ed9c298e
Callumf222 <callumfry57@gmail.com>
2022-11-24 11:48:23 +0000

    Ready for implementation on mobile
    Basic implementation is ready and many improvements can be made but it should be fully functional

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -8,24 +8,28 @@ export default function AboutMe() {
 
       <a id='AboutMePage'></a>
       <section className='AboutMe'>
-          <div className='profilePicture'></div>
             <h1 className='hAboutMe'>About Me</h1>
-            <div className='pAboutMe'>
 
-              <p>Hello, my name is Callum.</p>
+            <div className='contentHolder'>
 
-              <p>I am currently studying Computer Science at the University of Portsmouth; subject areas that particularly interest me include programming, software engineering and cyber security.</p>
-               
-               
-               <p>Throughout my first year I built on my communication and teamwork skills while leading a number of group projects.</p>
+              <div className='profilePicture'></div>
+              <div className='pAboutMe'>
+
+                <p>Hello, my name is Callum.</p>
+
+                <p>I am currently studying Computer Science at the University of Portsmouth; subject areas that particularly interest me include programming, software engineering and cyber security.</p>
+                
+                
+                <p>Throughout my first year I built on my communication and teamwork skills while leading a number of group projects.</p>
+                
+                
+                <p>In my spare time I enjoy playing games, watching football and coding; comfortable in Java, Python, HTML, CSS, Javascript, Dart, C# and SQL.</p>
               
-              
-              <p>In my spare time I enjoy playing games, watching football and coding; comfortable in Java, Python, HTML, CSS, Javascript, Dart, C# and SQL.</p>
-            
-              <p>Feel free to check out some of my projects below; following my progress in becoming a developer.</p>
+                <p>Feel free to check out some of my projects below; following my progress in becoming a developer.</p>
 
 
-          </div>
+              </div>
+            </div>
       </section>
 
     </>

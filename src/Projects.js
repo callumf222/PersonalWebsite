674141e30c4b98ffaba2b9875ef3939b6e6be7e4
Callumf222 <callumfry57@gmail.com>
2022-11-24 08:55:42 +0000

    Small touches
    attempting to get it ready to upload with finishing details

--- src/Projects.js
+++ src/Projects.js
@@ -4,7 +4,7 @@ export default function Project() {
 
   return (
     <>
-
+      <a id='ProjectPage'></a>
       <div className='backgroundProjects'>
         <div className='spacer layer1'>
           
@@ -15,10 +15,10 @@ export default function Project() {
          
 
           <div className='projectImages'>
-              <div className='projectIMG'> <a href='/' className='projectImage1'> </a> <h2 className='projectName'>Personal Website</h2></div>
-              <div className='projectIMG'> <a href='/' className='projectImage2'> </a> <h2 className='projectName'>Scheduling App</h2> </div>
-              <div className='projectIMG'> <a href='/' className='projectImage3'> </a> <h2 className='projectName'>Gaming Landing Page</h2></div>
-              <div className='projectIMG'> <a href='/' className='projectImage4'> </a> <h2 className='projectName'>Netflix Clone</h2></div>
+              <div className='projectIMG'> <a href='/' className='projectImage1' alt='personal website'> </a> <h2 className='projectName'>Personal Website</h2></div>
+              <div className='projectIMG'> <a href='/' className='projectImage2' alt='scheduling book'> </a> <h2 className='projectName'>Scheduling App</h2> </div>
+              <div className='projectIMG'> <a href='/' className='projectImage3' alt='two people playing a game'> </a> <h2 className='projectName'>Gaming Site</h2></div>
+              <div className='projectIMG'> <a href='/' className='projectImage4' alt='Netflix on laptop'> </a> <h2 className='projectName'>Netflix Clone</h2></div>
               <div className='projectIMG'> <a href='/' className='projectImage5'> </a> <h2 className='projectName'>Coming Soon</h2></div>
           </div>
 
